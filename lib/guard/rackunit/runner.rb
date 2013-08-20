a510a23348b3f8e8e4e0569de14503d2c426efbd
Chad Albers <calbers@neomantic.com>
2013-08-20 21:37:55 +0900

    more autoload work around

--- lib/guard/rackunit/runner.rb
+++ lib/guard/rackunit/runner.rb
@@ -1,19 +1,20 @@
 require 'guard/guard'
 
-module Support
+module Guard
+  class Rackunit
 
-  class Runner
+    class Runner
 
-    def initialize(run_file_name)
-      @run_file_name = run_file_name
-    end
+      def initialize(run_file_name)
+        @run_file_name = run_file_name
+      end
 
-    def run
-      success = system("racket #{@run_file_name}")
-      if !success
-        Guard::Notifier.notify("Failed", :title => 'Rackunit results')
+      def run
+        success = system("racket #{@run_file_name}")
+        if !success
+          Guard::Notifier.notify("Failed", :title => 'Rackunit results')
+        end
       end
     end
   end
 end
-
