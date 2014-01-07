a604568417fc5a4183d698766344d7ce9b568f40
Chad Albers <calbers@neomantic.com>
2014-01-07 20:26:50 +0900

    added spec_helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,14 @@
+# Require this file using `require "spec_helper"` to ensure that it is only
+# loaded once.
+# See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+require 'rspec'
+require 'rspec/mocks'
+Dir["./spec/support/**/*.rb"].sort.each {|f| require f}
+
+RSpec.configure do |config|
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+  config.order = 'random'
+end
