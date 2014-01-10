5c3f48432741f72923f434975053c45d71c1f3a7
Chad Albers <calbers@neomantic.com>
2014-01-10 20:20:36 +0900

    spec for command

--- spec/lib/guard/rack_unit/command_spec.rb
+++ spec/lib/guard/rack_unit/command_spec.rb
@@ -2,22 +2,28 @@ require 'spec_helper'
 
 describe Guard::RackUnit::Command do
 
+  let(:instance){described_class.new}
+
   it 'returns failure result when it is unsuccessful' do
     stub_failed_run do
-      result = described_class.new.execute(['paths'])
-      expect(result).to be_instance_of Guard::RackUnit::RunResult::Failure
+      expect(instance.execute(['paths'])).
+        to be_instance_of Guard::RackUnit::RunResult::Failure
     end
   end
 
   it "returns a success result when it is successful" do
     stub_successful_run do
-      result = described_class.new.execute(['paths'])
-      expect(result).to be_instance_of Guard::RackUnit::RunResult::Success
+      expect(instance.execute(['paths'])).
+        to be_instance_of Guard::RackUnit::RunResult::Success
     end
   end
 
   it "returns pending result if there are no are no paths" do
-    result = described_class.new.execute([])
-    expect(result).to be_instance_of Guard::RackUnit::RunResult::Pending
+    expect(instance.execute([])).to be_instance_of Guard::RackUnit::RunResult::Pending
+  end
+
+  it "runs the correct command" do
+    expect(Open3).to receive(:popen3).with("raco test paths1 paths2")
+    instance.execute(['paths1', 'paths2'])
   end
 end
